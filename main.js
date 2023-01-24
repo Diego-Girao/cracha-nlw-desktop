b98eedfd57461b92e2879fc1d38d13075ecf6fe0
diegogirao <diegoramiro25@gmail.com>
2023-01-24 18:11:59 -0300

    ajuste no linkedin em linkSocialMedia

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 const linksSocialMedia = {
-    github: "Diego-Girao",
-    youtube: "diegoramiro25",
-    twitch: "diegoramiro25",
-    linkedin: "diego-ramiro-santos-girão-1ab65bbb"
+	github: "Diego-Girao",
+	youtube: "diegoramiro25",
+	twitch: "diegoramiro25",
+	linkedin: "diego-girao",
 }
 
 function replaceSocialMediaLinks() {
-    for ( let li of socialLinks.children) {
-        const social = li.getAttribute('class')
+	for (let li of socialLinks.children) {
+		const social = li.getAttribute("class")
 
-        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
-        
-    }
+		li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+	}
 }
 replaceSocialMediaLinks()
 
 function getGitHubProfileInfos() {
-    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+	const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        userName.textContent = data.name
-        userBio.textContent = data.bio
-        userLink.href = data.html_url
-        userPhoto.src = data.avatar_url
-        userLogin.textContent = data.login
-    })
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			userName.textContent = data.name
+			userBio.textContent = data.bio
+			userLink.href = data.html_url
+			userPhoto.src = data.avatar_url
+			userLogin.textContent = data.login
+		})
 }
 
 getGitHubProfileInfos()
